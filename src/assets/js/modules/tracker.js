2319d6645a252642175453456ce57eb0125c46e7
Alex Zappa <alex@zappa.dev>
2023-11-09 12:00:12 +0100

    update redirects and fix plausible tracking

--- src/assets/js/modules/tracker.js
+++ src/assets/js/modules/tracker.js
@@ -4,7 +4,7 @@ import colorScheme from "./_detectColorScheme";
 
 const {trackEvent, trackPageview, enableAutoOutboundTracking} = Plausible({
   domain: "serverless-gems.dev",
-  apiHost: "https://serverless-gems.netlify.app",
+  apiHost: "https://plausible.serverless-gems.dev",
 });
 
 document.addEventListener("DOMContentLoaded", () => {
